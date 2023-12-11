1f36d899d6af2cf73904363e03a96db37f1ad8f1
la la <tranphan2910@gmail.com>
2023-12-11 21:57:48 +0700

    update

---
+++ Dragon Tale/src/Handlers/Content.java
@@ -0,0 +1,41 @@
+package Handlers;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Content {
+    public static BufferedImage[][] Slugger = load("Resources/Sprites/Enemies/slugger.gif", 30, 30);
+    public static BufferedImage[][] SluggerOnFire = load("Resources/Sprites/Enemies/sluggeronfire.gif", 30, 30);
+    public static BufferedImage[][] Arachnik = load("Resources/Sprites/Enemies/arachnik.gif", 30, 30);
+    public static BufferedImage[][] Explosion = load("Resources/Sprites/Enemies/explosion.gif", 30, 30);
+    public static BufferedImage[][] ArachnikOnFire = load("Resources/Sprites/Enemies/arachnikonfire.png", 30, 30);
+    public static BufferedImage[][] HatMonkey = load("Resources/Sprites/Enemies/hatmonkeymoving.png", 30, 30);
+    public static BufferedImage[][] Coin = load("Resources/Sprites/coin.png", 30, 30);
+    public static BufferedImage[][] Portal = load("Resources/Sprites/portal.gif", 40, 40);
+
+
+    public static BufferedImage[][] load(String s, int w, int h) {
+        BufferedImage[][] ret;
+        try {
+            BufferedImage spritesheet = ImageIO.read(
+                    new File(s)
+            ); //ImageIO.read(Content.class.getResourceAsStream(s));
+            int width = spritesheet.getWidth() / w;
+            int height = spritesheet.getHeight() / h;
+            ret = new BufferedImage[height][width];
+            for(int i = 0; i < height; i++) {
+                for(int j = 0; j < width; j++) {
+                    ret[i][j] = spritesheet.getSubimage(j * w, i * h, w, h);
+                }
+            }
+            return ret;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("Error loading graphics.");
+            System.exit(0);
+        }
+        return null;
+    }
+}
